6bc1847fefdc8baf6fffec78ed5896151831ea81
Administrator <Administrator@pc-PC>
2016-02-16 09:39:54 +0800

    update

--- Hdoc/html/js/template.js
+++ Hdoc/html/js/template.js
@@ -16,9 +16,9 @@ var header_list={"header_list":[
     {"first_list_href":"front_businessPolicy.html",
      "first_list_name":"创业政策",
      "second":[
-        {"second_list_href":"#","second_list_name":"学校"},
-          {"second_list_href":"#","second_list_name":"地方"},
-          {"second_list_href":"#","second_list_name":"国家"}
+        {"second_list_href":"front_school.html","second_list_name":"学校"},
+          {"second_list_href":"front_place.html","second_list_name":"地方"},
+          {"second_list_href":"front_country.html","second_list_name":"国家"}
      ]
     },
     {"first_list_href":"front_team_create.html",
@@ -30,7 +30,7 @@ var header_list={"header_list":[
     {"first_list_href":"front_match_information.html",
      "first_list_name":"比赛信息",
      "second":[
-        {"second_list_href":"front_past_activities.html","second_list_name":"往期活动"},
+        {"second_list_href":"front_past_activities.html","second_list_name":"往期活动"}
          
      ]
     }
@@ -39,65 +39,23 @@ var header_list={"header_list":[
 ]};
 
 
-var footer_list={"footer_list":[
-    {
+
+var footer_list={
+    "footer_list":{
         "second":[
-        {
-        "second_url":"javascript:void(0)",
-            "second_name":"链接"
-        },{
-        "second_url":"http://www.scu.edu.cn",
-            "second_name":"四川大学"
-        }      
-         ]
-    }
-, {
-        "second":[ {
-        "second_url":"javascript:void(0)",
-            "second_name":"链接"
-        },{
-        "second_url":"http://www.xuetangx.com",
-            "second_name":"学堂在线"
-        }
-                 
-         ]
-    }
-, {
-        "second":[
-             {
-        "second_url":"javascript:void(0)",
-            "second_name":"链接"
-        },{
-        "second_url":"http://36kr.com",
-            "second_name":"36氪"
-        }
-                 
-         ]
-    }
-, {
-        "second":[ {
-        "second_url":"javascript:void(0)",
-            "second_name":"链接"
-        },{
-        "second_url":"http://www.x-lab.tsinghua.edu.cn",
-            "second_name":"清华X-LAB"
-        }        
-         ]
-    }
-, {
-        "second":[
-             {
-        "second_url":"javascript:void(0)",
-            "second_name":"链接"
-        },{
-        "second_url":"http://weixin.tccxfw.com",
-            "second_name":"四川省科学技术厅"
-        }
-                 
-         ]
+        {"second_list_href":"javascript:void(0);","second_list_name":"关于我们"},
+        {"second_list_href":"javascript:void(0);","second_list_name":"四川大学"},
+        {"second_list_href":"javascript:void(0);","second_list_name":"学堂在线"}
+         ],
+        "third":[
+            {"third_list_href":"javascript:void(0);","third_list_name":"36氪/关注互联网创业"},
+            {"third_list_href":"javascript:void(0);","third_list_name":"清华X-LAB"},
+            {"third_list_href":"javascript:void(0);","third_list_name":"四川省科学技术厅"}
+        ]
     }
 
-]}
+
+};
 
 
 //使用baidu.template命名空间
@@ -108,6 +66,7 @@ var header_temp=bt_1('header_temp',header_list);
 $('.navContent').html(header_temp);
 
 var bt_2=baidu.template;
-var footer_temp=bt_2('footer_temp',footer_list);
 
-$('.footer-bag').html(footer_temp);
+var footer_temp=bt_2('footer_temp',footer_list);
+console.log(footer_temp)
+$('#f_center').html(footer_temp);
