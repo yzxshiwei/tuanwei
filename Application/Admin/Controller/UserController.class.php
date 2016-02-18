e58d2271813c025ea374da0b45961df24f5be1ef
游宗学 <710414018@qq.com>
2016-02-18 17:01:28 +0800

    后台注册

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -12,4 +12,40 @@ class UserController extends Controller{
         \Common\Helper\RunUser::newInstantiation()->signOut();
         $this->success('退出成功',U('Home/Index/Index'));
     }
+    /**
+     * 用户注册
+     * 添加时间2016-2-16
+     *
+     * @author yzx
+     */
+    public function register(){
+        if (IS_POST){
+            $userModel = new \Common\Model\UsersModel();
+            $input_data = array();
+            $input_data['email'] = I('post.email','','string');
+            $input_data['card_type'] = I('post.card_type','string');
+            $input_data['card_id'] = I('post.card_id',0,'intval');
+            $input_data['user_type'] = I('post.user_type',0,'intval');
+            $input_data['sex'] = I('post.sex',0,'intval');
+            $input_data['passwd'] = I('post.passwd',''.'string');
+            $input_data['college'] = I('post.college','','string');
+            $input_data['user_name'] = I('post.user_name','','string');
+            $input_data['nation'] = I('post.nation','','string');
+            $input_data['birth'] = I('post.birth','','string');
+            $input_data['major'] = I('post.major',0,'intval');
+            $input_data['degree'] = I('post.degree',0,'intval');
+            $result = $userModel->addUser($input_data);
+            if ($result['status']){
+                $this->success('注册成功',U('Index/usermanage'));
+            }else {
+                $this->error($result['msg']);
+            }
+        }else {
+            $major = \Common\Model\UsersModel::$major;
+            $degree = \Common\Model\UsersModel::$degree;
+            $this->assign('major',$major);
+            $this->assign('degree',$degree);
+            $this->display();
+        }
+    }
 }
