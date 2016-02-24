85b96e362c3e5a43c8db6a3a1f9390fd928eba46
游宗学 <710414018@qq.com>
2016-02-24 10:31:38 +0800

    sdfdff

--- Application/Common/Model/TeamModel.class.php
+++ Application/Common/Model/TeamModel.class.php
@@ -19,4 +19,20 @@ class TeamModel extends \Common\Helper\Model{
      */
     public $user_type = array(self::USER_TYPE_MEMBER => '队员',
                               self::USER_TYPE_CAPTAIN => '队长');
+    /**
+     * 获取团队列表
+     * 添加时间2016-2-24
+     * 
+     * @author yzx
+     * @param unknown $user_id
+     * @return array
+     */
+    public function listData($user_id) {
+        $result = $this->field('p.name,team.id,u.user_name,u.create_time')
+             ->join('project as p on (team.project_id = p.id)','left')
+             ->join("users as u on (team.user_id = u.user_id)",'left')
+             ->where(array('user_id'=>$user_id,'user_type' => self::USER_TYPE_CAPTAIN))
+             ->select();
+        return $result;
+    }
 }
