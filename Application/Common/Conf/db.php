b7a5c662d1bd289c535825dcaa1916fe0e2c038f
游宗学 <710414018@qq.com>
2016-02-24 15:37:54 +0800

    数据库配置

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -7,8 +7,8 @@
  */
 return array(
     "DB_TYPE"=>"mysql",
-    "DB_HOST"=>"127.0.0.1",
+    "DB_HOST"=>"192.168.31.21",
     "DB_NAME"=>"tuanwei",
     "DB_USER"=>"root",
-    "DB_PWD"=>"root"
+    "DB_PWD"=>"123456"
 );
