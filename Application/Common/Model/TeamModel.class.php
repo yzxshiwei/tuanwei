815a2d4af67831bf9f9c49ecb0d1adae0153dd22
游宗学 <710414018@qq.com>
2016-02-24 10:05:37 +0800

    commint

--- Application/Common/Model/TeamModel.class.php
+++ Application/Common/Model/TeamModel.class.php
@@ -2,4 +2,21 @@
 namespace Common\Model;
 class TeamModel extends \Common\Helper\Model{
     protected $tableName = 'team';
+    /**
+     * 用户类型
+     * 添加时间2016-2-24
+     * 
+     * @author yzx
+     * @var unknown
+     */
+    const USER_TYPE_MEMBER = 'member';
+    const USER_TYPE_CAPTAIN = 'captain';
+    /**
+     * 添加时间2016-2-24
+     * 
+     * @author yzx
+     * @var array
+     */
+    public $user_type = array(self::USER_TYPE_MEMBER => '队员',
+                              self::USER_TYPE_CAPTAIN => '队长');
 }
