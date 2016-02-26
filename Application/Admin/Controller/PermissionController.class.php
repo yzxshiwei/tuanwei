7386eec2eb889cdbda510ad188c1d5623ff938f6
游宗学 <710414018@qq.com>
2016-02-26 14:24:33 +0800

    权限控制器

---
+++ Application/Admin/Controller/PermissionController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Admin\Controller;
+/**
+ * 用户权限控制器
+ * 添加时间2016-2-26
+ * 
+ * @author yzx
+ * @author Administrator
+ *
+ */
+class PermissionController extends Controller{
+    
+}
