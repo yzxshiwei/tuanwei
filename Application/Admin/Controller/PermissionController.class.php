0e5c6b752522e9a72107eb9e37634979a51d1a21
游宗学 <710414018@qq.com>
2016-02-26 18:03:48 +0800

    权限添加页面

--- Application/Admin/Controller/PermissionController.class.php
+++ Application/Admin/Controller/PermissionController.class.php
@@ -5,9 +5,47 @@ namespace Admin\Controller;
  * 添加时间2016-2-26
  * 
  * @author yzx
- * @author Administrator
  *
  */
 class PermissionController extends Controller{
-    
+    /**
+     * 添加分组
+     * 添加时间2016-2-26
+     * 
+     * @author yzx
+     */
+    public function addgroup() {
+        $this->display();
+    }
+    /**
+     * 分组列表
+     * 添加时间2016-2-26
+     * 
+     * @author yzx
+     */
+    public function grouplist() {
+        $this->display();
+    }
+    /**
+     * 添加权限
+     * 添加时间2016-2-26
+     * 
+     * @author yzx
+     */
+    public function addpermission() {
+        $this->display();
+    }
+    /**
+     * 用户添加分组
+     * 添加时间2016-2-26
+     * 
+     * @author yzx
+     */
+    public function usergroup() {
+        $user_id = I('user_id',0,'intval');
+        $userModel = new \Common\Model\UsersModel();
+        $result = $userModel->where(array('user_id'=>$user_id))->find();
+        $this->assign('data',$result);
+        $this->display();
+    }
 }
