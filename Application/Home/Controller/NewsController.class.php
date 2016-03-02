50adc6d6db8457f4fcdc227ec78d43849c1d53f1
mt961313085 <961313085@qq.com>
2016-03-02 16:48:39 +0800

    创业动态分页样式调整

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -69,29 +69,29 @@ class NewsController extends \Common\Helper\Controller{
     	$dataTop = $News->field('id,img_url,title')->where("col=2 and flag=1 and $timestamp > top_s AND $timestamp < top_e")->order('public_t desc')->limit(5)->select();
     	$count = $News->field('id,title')->where("col=2 and flag=1 and $timestamp > top_s AND $timestamp < top_e")->order('public_t desc')->limit(5)->count();
     	
-    	if($count<5){
-    		$dataNormal = $News->field('id,img_url,title')->where("col=2 and flag=1 and top_s=0 and top_e=0")->order('public_t desc')->limit(5-$count)->select();
+    	$dataNormal = $News->field('id,img_url,title')->where("col=2 and flag=1 and top_s=0 and top_e=0")->order('public_t desc')->limit(5-$count)->select();
     	
-	    	if(!is_null($dataNormal)){
-	    		$data = array_merge($dataTop, $dataNormal);
-	    	}
-	    	else {
-	    		//该栏目没有非置顶新闻
-	    		$data = $dataTop;
-	    	}
-	    	
-    	}
-    	else{
-    		$data = $dataTop;
-    	}
+    	$p = I('get.p',1,'intval');
     	
-    	$img_url = array();
+    	$data = array_merge((array)$dataTop, (array)$dataNormal);
+    	
+    	//分页
+    	$page = new \Think\Page(count($data),1);
+    	$show = $page->show();
+    	//轮播图
+    	$i = 0;
     	foreach ($data as $k => $v){
     		if($v['img_url']!= '0'){
-    			$img_url[] = array('id' => $v['id'],'img_url' => $v['img_url']);
+    			if($i == 6){
+    				break;
+    			}
+    			$img_url[$i] = array('id' => $v['id'],'img_url' => $v['img_url']);
+				$i++;
     		}
-    		
     	}
+    	
+    	$this->assign('page', $show);
+    	$data = array_slice($data, (--$p)*5, 1);
     	$this->assign('img_url',$img_url);
     	$this->assign ( 'data', $data );
 		$this->display ();
