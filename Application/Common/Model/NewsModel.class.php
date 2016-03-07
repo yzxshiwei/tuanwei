454d08fef19daaef431a766a2433183cd56fa373
yingzi1212 <389843954@qq.com>
2016-03-07 14:01:33 +0800

    修复bug

--- Application/Common/Model/NewsModel.class.php
+++ Application/Common/Model/NewsModel.class.php
@@ -9,6 +9,7 @@ class NewsModel extends \Common\Helper\Model{
     const COL_3 = 3;
     const COL_4 = 4;
     const COL_5 = 5;
+	const COL_6 = 6;
     
     const SUB_COL_1 = 1;
     const SUB_COL_2 = 2;
@@ -24,6 +25,7 @@ class NewsModel extends \Common\Helper\Model{
         self::COL_3 => '讲座培训',
         self::COL_4 => '学院风采',
         self::COL_5 => '资料库',
+        self::COL_6 => '场地类别',
     );
     /**
      * 新闻副栏目
