4768eb0d19e24c5de0ac849758d54d66ce159d75
yingzi1212 <389843954@qq.com>
2016-03-10 21:19:37 +0800

    创建比赛，附件 可上传可不上传

--- Application/Admin/Controller/MatchController.class.php
+++ Application/Admin/Controller/MatchController.class.php
@@ -24,53 +24,66 @@ class MatchController extends Controller{
 			//开启事务
 		    $match->startTrans();
 
-			$file_res1 = Upload($_FILES['selectFiles']);
-            $file_res2 = Upload($_FILES['upload_template']);
-//			$file_res3 = Upload($_FILES['upload_reg_page']);
-			
-            if (!$file_res1['status'] || !$file_res2['status']){
-                $this->error($file_res1['msg']);
-            }else {
-				$data = array();
-				$data["name"] = I('post.name','','string');
-				$data["sub_title"] = I('post.sub_title','','string');
-				$data["project_start_time"] = strtotime(I('post.race-start-date','','string'));
-				$data["project_end_time"] = strtotime(I('post.race-end-date','','string'));
-				$data["judge_amount"] = I("post.amount","","string");
-				$data["rules"] = htmlspecialchars(I('post.editorValue'));
-				$data["sign_start_time"] = strtotime(I("post.reg-start-date",'','string'));
-				$data["sign_end_time"] = strtotime(I("post.reg-end-date","","string"));
-				$data["cover_src"] = $file_res1['file_path'];
-				$data["start_file_src"] = $file_res2['file_path'];
-//				$data["template_src"] = $file_res3['file_path'];
-				
-				$relust = $match->add($data);
-				
-				if($relust){
-					
-					$teacherid = I("post.teacherid");
-                    $packet = I("post.packet");
-					//添加评委老师
-					$jugedsModel = new \Common\Model\JudgesModel;
-					$j_res = $jugedsModel->addJudges($teacherid,$relust);
-					//添加比赛组名称
-					$packetModel = new \Common\Model\PacketModel;
-					$p_res = $packetModel->addName($packet,$relust);
-					
-				    if($j_res && $p_res){
-				    	$match->commit();
-				    	$messageModel = new \Common\Model\MessageModel();
-				    	$messageModel->sendMsg($teacherid, $this->user['user_id'], $messageModel::TYPE_JUDGES_PROJECT, '你有比赛评审邀请',$relust);
-				    	$this->success('添加比赛成功',U('Match/matchmanage'));
-				    }else{
-				    	$match->rollback();
-				    	$this->error('添加比赛失败');
-				    }
+			if($_FILES['selectFiles']['tmp_name']){
+				$file_res1 = Upload($_FILES['selectFiles']);
+				if($file_res1["status"]){
+					$data["cover_src"] = $file_res1['file_path'];
 				}else{
-				    $match->rollback();
-                    $this->error('添加比赛失败');
+					$this->error($file_res1['msg']);
 				}
-            }
+			}
+			
+			if($_FILES['upload_template']['tmp_name']){
+				$file_res2 = Upload($_FILES['upload_template']);
+				if($file_res2["status"]){
+					$data["start_file_src"] = $file_res2['file_path'];
+				}else{
+					$this->error($file_res2['msg']);
+				}
+			}
+
+//			$file_res1 = Upload($_FILES['selectFiles']);
+//          $file_res2 = Upload($_FILES['upload_template']);
+//			$file_res3 = Upload($_FILES['upload_reg_page']);
+			$data = array();
+			$data["name"] = I('post.name','','string');
+			$data["sub_title"] = I('post.sub_title','','string');
+			$data["project_start_time"] = strtotime(I('post.race-start-date','','string'));
+			$data["project_end_time"] = strtotime(I('post.race-end-date','','string'));
+			$data["judge_amount"] = I("post.amount","","string");
+			$data["rules"] = I('post.editorValue');
+			$data["sign_start_time"] = strtotime(I("post.reg-start-date",'','string'));
+			$data["sign_end_time"] = strtotime(I("post.reg-end-date","","string"));
+//				$data["cover_src"] = $file_res1['file_path'];
+//				$data["start_file_src"] = $file_res2['file_path'];
+//				$data["template_src"] = $file_res3['file_path'];
+			$relust = $match->add($data);
+			
+			if($relust){
+				
+				$teacherid = I("post.teacherid");
+                $packet = I("post.packet");
+				//添加评委老师
+				$jugedsModel = new \Common\Model\JudgesModel;
+				$j_res = $jugedsModel->addJudges($teacherid,$relust);
+				//添加比赛组名称
+				$packetModel = new \Common\Model\PacketModel;
+				$p_res = $packetModel->addName($packet,$relust);
+				
+			    if($j_res && $p_res){
+			    	$match->commit();
+			    	$messageModel = new \Common\Model\MessageModel();
+			    	$messageModel->sendMsg($teacherid, $this->user['user_id'], $messageModel::TYPE_JUDGES_PROJECT, '你有比赛评审邀请',$relust);
+			    	$this->success('添加比赛成功',U('Match/matchmanage'));
+			    }else{
+			    	$match->rollback();
+			    	$this->error('添加比赛失败');
+			    }
+			}else{
+			    $match->rollback();
+                $this->error('添加比赛失败');
+			}
+            
 		}else{
 			$teacher_list = $userModel->where(array('user_type'=>\Common\Model\UsersModel::TYPE_TEACHER))->field("user_id,user_name")->select();
 			$prolist = $project->field("id,name")->select();
