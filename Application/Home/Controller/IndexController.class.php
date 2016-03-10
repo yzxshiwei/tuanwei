8fe17d795e923c3b5404b837b3b3c0ce33d7a175
yingzi1212 <389843954@qq.com>
2016-03-10 14:32:42 +0800

    首页新闻  置顶新闻不够5条  查5条

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,13 +6,20 @@ class IndexController extends \Common\Helper\Controller {
 		$newModel = new \Common\Model\NewsModel;
 		
 		$where["flag"] = 1;
-	    $where["top_s"] = array("elt",time());
-		$where["top_e"] = array("egt",time());
+		$time = time();
+	    $where["top_s"] = array("elt",$time);
+		$where["top_e"] = array("egt",$time);
+
 		$newList = $newModel->where($where)->order("public_t desc")->field("id,title,img_url")->limit(5)->select();
-		if(!$newList){
-			$newList = $newModel->where(array("flag"=>1))->order("public_t desc")->field("id,title,img_url")->limit(5)->select();
-		}
 		
+		if(count($newList)<5 && count($newList)>0 ){
+			
+			$num = 5-count($newList);
+		    $newList2 = $newModel->where("(top_s > $time or top_e<$time ) and flag=1")->order("public_t desc")->field("id,title,img_url")->limit($num)->select();
+			$newList = array_merge($newList,$newList2);
+		}else{
+		   $newList = $newModel->where(array("flag"=>1))->order("public_t desc")->field("id,title,img_url")->limit(5)->select();
+		}
 		$teamModel = new \Common\Model\TeamModel;
 		$utype = \Common\Model\TeamModel::USER_TYPE_CAPTAIN;
 		$teamList = $teamModel->where(array("user_type"=>$utype))->order("id desc")->limit(4)->select();
