c4cfa45881e42bc40a87f414cbf728e4f14bbb55
yingzi1212 <389843954@qq.com>
2016-03-10 17:49:59 +0800

    用户注册 投资人等  管理员审核

--- Application/Common/Helper/Message.class.php
+++ Application/Common/Helper/Message.class.php
@@ -18,16 +18,16 @@ class Message{
         $msg_list = $messageModel->where($where)->select();
         if (is_array($msg_list) && !empty($msg_list)){
             foreach ($msg_list as $k => $v){
-                if ($v['msg_type'] == $messageModel::TYPE_JUDGES_PROJECT || 
-                    $v['msg_type'] == $messageModel::TYPE_TEACHER_PROJECT ||
-                    $v['msg_type'] == $messageModel::TYPE_USER_PROJECT){
-                    $msg_list[$k]['agree_url'] = U('Index/agree',array('id'=>$v['id'],'proid' => $v['project_id']));
-                    $msg_list[$k]['refuse_url'] = U('Index/refuse',array('id'=>$v['id'],'proid' => $v['project_id']));
-                    $msg_list[$k]['is_sys'] = 1;
-                }else {
-                    $msg_list[$k]['is_sys'] = 0;
-                    $msg_list[$k]['read_url'] = U('Index/read',array('id'=>$v['id']));
-                }
+            	switch($v['msg_type']){
+            		case $messageModel::TYPE_JUDGES_PROJECT || $messageModel::TYPE_TEACHER_PROJECT || $messageModel::TYPE_USER_PROJECT || $messageModel::TYPE__APPLY :
+            		    $msg_list[$k]['agree_url'] = U('Index/agree',array('id'=>$v['id'],'proid' => $v['project_id']));
+                        $msg_list[$k]['refuse_url'] = U('Index/refuse',array('id'=>$v['id'],'proid' => $v['project_id']));
+                        $msg_list[$k]['is_sys'] = 1;
+						break;
+					default:
+					    $msg_list[$k]['is_sys'] = 0;
+                        $msg_list[$k]['read_url'] = U('Index/read',array('id'=>$v['id']));
+            	}
             }
         }
         return $msg_list;
