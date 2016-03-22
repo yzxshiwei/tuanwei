0cf52408e1d83d9cef22a3dad15624b55d9776de
mt961313085 <961313085@qq.com>
2016-03-22 14:20:55 +0800

    分数排序2

--- Application/Common/Model/Match_ProjectModel.class.php
+++ Application/Common/Model/Match_ProjectModel.class.php
@@ -10,11 +10,12 @@ class Match_ProjectModel extends \Common\Helper\Model{
      * @param int $matchId
      * @return Ambigous <mixed, boolean, string, NULL, multitype:, unknown, object>
      */
-    public function listData($matchId) {
+    public function listData($matchId, $order) {
         $result = $this->field("p.name,p.id,match_project.match_id,ps.score")
         ->join("project as p on (p.id = match_project.project_id)","left")
         ->join('project_status as ps on(ps.project_id = p.id)','left')
         ->where(array('match_id' => $matchId))
+        ->order($order)
         ->select();
         return $result;
     }
