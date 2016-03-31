e6cc0127d7bb7ccfc643e3815ad3b26e21056343
yingzi1212 <389843954@qq.com>
2016-03-31 13:19:35 +0800

    a

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -35,48 +35,43 @@ return array(
                               'ico' => 'fa fa-lightbulb-o',
 							  'map' =>array("createnews")
 							  ),
-        
+       "newsmanage" => array('name' => "信息管理",
+                              'url' => 'News/newsmanage',
+                              'ico' => 'fa fa-cubes',
+							  'map' =>array("newsmanage","newedit")
+							  ),
         "creatematch" => array('name' => "创建比赛",
                                'url' => 'Match/creatematch',
                                'ico' => 'fa  fa-graduation-cap',
 							   'map' => array("creatematch")
 							   ),
-        
-        "createproject" => array('name' => "创建项目",
-                                 'url' => 'Project/createproject',
-                                 'ico' => 'fa fa-bookmark-o',
-								 'map' => array("createproject")
-								 ),
-        
         "matchmanage" => array('name' => "比赛管理",
                                'url' => 'Match/matchmanage',
                                'ico' => 'fa fa-futbol-o',
                                'map' => array("matchmanage","editmatch","viewmatch")
 							   ),
-        
-        "newsmanage" => array('name' => "信息管理",
-                              'url' => 'News/newsmanage',
-                              'ico' => 'fa fa-cubes',
-							  'map' =>array("newsmanage","newedit")
-							  ),
-        
-//      "teamdiscuss" => array('name' => "团队交流",
-//                             'url' => 'Team/teamdiscuss',
-//                             'ico' => 'fa fa-language',
-//							   'map' =>array("teamdiscuss")
-//							   ),
-        
+        "createproject" => array('name' => "创建项目",
+                                 'url' => 'Project/createproject',
+                                 'ico' => 'fa fa-bookmark-o',
+								 'map' => array("createproject")
+								 ),
         "projectmanage" => array('name' => "项目管理",
                                  'url' => 'Project/projectmanage',
                                  'ico' => 'fa fa-share-alt',
 								 'map' => array("projectmanage","workreview","editproject")
 								 ),
         
-        "grouplist" => array('name' => "分组列表",
+        "grouplist" => array('name' => "权限分配",
                              'url' => 'Permission/grouplist',
                              'ico' => 'fa fa-bars',
 							 'map' =>array("grouplist","addpermission","usergroup")
 							 )
+
+//      "teamdiscuss" => array('name' => "团队交流",
+//                             'url' => 'Team/teamdiscuss',
+//                             'ico' => 'fa fa-language',
+//							   'map' =>array("teamdiscuss")
+//							   ),
         
 //      "addgroup" => array('name' => "创建分组",
 //                          'url' => 'Permission/addgroup',
