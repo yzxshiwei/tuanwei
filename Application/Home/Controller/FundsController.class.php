5c4c9843a2547cbf569bdda29dcc87abd6343dcf
yingzi1212 <389843954@qq.com>
2016-04-21 13:18:14 +0800

    a

---
+++ Application/Home/Controller/FundsController.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace Home\Controller;
+class FundsController extends \Common\Helper\Controller {
+
+    /**
+     * 资金申请
+     * @return [type] [description]
+     */
+	public function funds(){
+        $Match = M('match');
+    	$timestamp = time();
+    	$day = date("Y-m-d",$timestamp);
+    	$data = $Match->field('id, name, sub_title, cover_src, start_file_src,template_src,sign_start_time,sign_end_time,project_start_time,project_end_time')->where("state=1")->order('id desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+
+    	foreach ($data as $k => $v){
+    		if($v["sign_start_time"] <= strtotime($day) && $v["sign_end_time"] > strtotime($day)){
+    			$data[$k]["times"] = TRUE;
+    		}else{
+    			$data[$k]["times"] = FALSE;
+    		}
+    	}
+    	$img_url = $Match->field('id, cover_src')->where("cover_src is not null and state=1 AND $timestamp < project_end_time")->limit(5)->select();
+    	$count = $Match->where("state=1 AND $timestamp < project_end_time")->count();
+    	//分页
+    	$page = new \Think\Page($count, 5);
+    	$show = $page->show();
+		$flag = $this->user["user_type"]==\Common\Model\UsersModel::TYPE_MANAGE?FALSE:TRUE;
+    	$this->assign('img_url',$img_url);
+		$this->assign('flag',$flag);
+    	$this->assign('data', $data);	
+    	$this->assign('page', $show);
+        $this->display();
+	}
+}
